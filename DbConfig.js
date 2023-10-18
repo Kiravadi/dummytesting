3a95ae7276302ccc49242b757180c1bd31f4f947
Murugesh Kiravadi <55143259+Kiravadi@users.noreply.github.com>
2023-10-18 16:52:11 +0530

    uploading GbConfig file

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cjsmkngtg7q1.ap-south-1.rds.amazonaws.com',
+    DB_USER : ‘admin’,
+    DB_PWD : ‘murugesh’,
+    DB_DATABASE : ‘webappdb’
+});
